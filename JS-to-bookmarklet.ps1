dd9603931cd5ef12c4529ca3eb08ae0de4195a13
gridlocdev <60323737+gridlocdev@users.noreply.github.com>
2021-06-24 12:17:54 -0700

    Added additional validation and warning messages

--- JS-to-bookmarklet.ps1
+++ JS-to-bookmarklet.ps1
@@ -17,15 +17,25 @@ if ($config.output_folder.EndsWith("/") -eq $false) {
 $inputFilePath = $config.input_folder + $fileName;
 $outputFilePath = $config.output_folder + "$($fileName).bookmarklet.txt";
 
+# Check to see if the script is a JavaScript file
+if ($config.script_name.Contains(".js") -eq $false) {
+    Write-Warning("ERROR: [[Non-JavaScript file detected]] Please supply a JavaScript file!");
+    Write-Host("`n" + "Press any key to close this window...");
+    $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+    Exit
+}
+
 # Test the file paths for validity
-if (Test-Path($inputFilePath)) {
-    # Check to see if the file is a JavaScript file
-    if ($inputFilePath.Contains(".js") -eq $false) {
-        Write-Warning("ERROR: [[Non-JavaScript file detected]] Please supply a JavaScript file!");
+if (Test-Path($config.input_folder)) {
+
+    # Check to see if the script_name is a valid file in that folder
+    if ((Test-Path($inputFilePath)) -eq $false) {
+        Write-Warning("ERROR: [[File not found in input folder]] Please verify the name of your script is correct in the script_name setting in settings.json.");
         Write-Host("`n" + "Press any key to close this window...");
         $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
         Exit
     }
+
     # Check to see if the file is empty
     if ((Get-Content($inputFilePath)).Length -eq 0) {
         Write-Warning("ERROR: [[Empty file detected]] Please add content to your JavaScript file! It'd be silly to create a bookmarklet that doesn't do anything.");
@@ -33,9 +43,16 @@ if (Test-Path($inputFilePath)) {
         $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
         Exit
     }
+    # Check to see if the output path is valid
+    if ((Test-Path($config.output_folder)) -eq $false) {
+        Write-Warning("ERROR: [[Output path not valid]] Please supply a valid folder path for the output_folder setting in settings.json.");
+        Write-Host("`n" + "Press any key to close this window...");
+        $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+        Exit
+    }
 }
 else {
-    Write-Warning("ERROR: [[File not found]] Please verify the name in the " + "$" + "fileName parameter is correct!");
+    Write-Warning("ERROR: [[Input path not valid]] Please supply a valid folder path for the output_folder setting in settings.json.");
     Write-Host("`n" + "Press any key to close this window...");
     $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
     Exit
@@ -51,8 +68,7 @@ New-Item($outputFilePath) | Out-Null
 $inputFile = Get-Content($inputFilePath);
 $inputFile_measuredText = Get-Content $inputFilePath | Measure-Object -Line
 
-if (($inputFile.StartsWith("Javascript:(function(){")) -and ($inputFile_measuredText.Lines -eq 1))
-{
+if (($inputFile.StartsWith("Javascript:(function(){")) -and ($inputFile_measuredText.Lines -eq 1)) {
     Write-Warning("ERROR: [[Bookmarklet detected]] This is probably already a Bookmarklet, or you've just got some funky syntax in this script.");
     Write-Host("`n" + "Press any key to close this window...");
     $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
